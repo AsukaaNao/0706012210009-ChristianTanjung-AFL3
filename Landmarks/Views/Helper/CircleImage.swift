6922a664cc68828a9d354b37d1a27487e824fb26
AsukaaNao <christiantanjung27@gmail.com>
2024-04-25 13:46:17 +0700

    chapter 4 done

--- Landmarks/Views/Helper/CircleImage.swift
+++ Landmarks/Views/Helper/CircleImage.swift
@@ -11,7 +11,7 @@ struct CircleImage: View {
     var image: Image
     
     var body: some View {
-        Image("turtlerock")
+        image
             .clipShape(/*@START_MENU_TOKEN@*/Circle()/*@END_MENU_TOKEN@*/)
             .overlay {
                 Circle().stroke(.white, lineWidth: 4)
